dcf66c8c2e532652ef89b1d76123a85d20b3d1e2
hyorimm <hyorimm@gmail.com>
2022-11-13 10:39:17 +0900

    docs:111322

---
+++ programmers/소수만들기.js
@@ -0,0 +1,22 @@
+function prime(k) {
+  if (k === 2) return true;
+  if (k % 2 === 0) return false;
+  if (k === 3) return true;
+  for (let i = 2; i * i <= k; i++) {
+    if (k % i === 0) return false;
+  }
+  return true;
+}
+
+function solution(nums) {
+  let answer = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      for (let k = j + 1; k < nums.length; k++) {
+        let key = nums[i] + nums[j] + nums[k];
+        if (prime(key)) answer++;
+      }
+    }
+  }
+  return answer;
+}
